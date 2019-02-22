de9c2036582be684aaa42a72517c8dfa136cf2fb
G50 <649867415@qq.com>
2019-02-22 08:32:59 +0800

    字典统计并存储单词个数

--- week1exam/five.py
+++ week1exam/five.py
@@ -12,30 +12,63 @@
 
 '''
 
+# d = {}
+# list0 = []
+# list1 = []
+# d2 = {}
+# s='The column above illustrates apparently' \
+#   ' the polularity of people ' \
+#   'doing exercise in a certain year ' \
+#   'from 2013 to 2018.Based upon the data,' \
+#   'we can see that python is wonderful. ' \
+#   'python is wonderful. Python '
+#
+# s1 = s.replace(",","").replace(".","")
+# list0 = s.split(" ",s.count(" ")-1)     #字符串结尾多一个空格，消除空格
+# print('list0',list0)
+#
+# for i in list0:
+#     d[i] = list0.count(i)   #生成字典并统计次数
+#     x = (i,list0.count(i))
+#     list1.append(x)
+# print('d:',d)
+# print("list1:",list1)
+#
+#
+# L = sorted(list1, key=lambda x:x[1])   #指定按那个元素进行排序
+# print(L)
+#
+# for i in L:
+#     j = i[0]
+#     d2[j] = i[1]
+# print('d2:',d2)
+
+#-----------------------------------
+#方法2：
+
 d = {}
 list0 = []
-list1 = []
+
+d2 = {}
 s='The column above illustrates apparently' \
   ' the polularity of people ' \
   'doing exercise in a certain year ' \
   'from 2013 to 2018.Based upon the data,' \
   'we can see that python is wonderful. ' \
-  'python is wonderful. Python ' \
+  'python is wonderful. Python '
 
 s1 = s.replace(",","").replace(".","")
-list0 = s.split(" ")
-#print(list0)
+list0 = s.split(" ",s.count(" ")-1)     #字符串结尾多一个空格，消除空格
+print('list0',list0)
 
 for i in list0:
     d[i] = list0.count(i)   #生成字典并统计次数
-    list1.append(d[i])
 print('d:',d)
 
-d2 = []
-for i in list1:
-    for j in d:
-        if list[i]==d[j]:
-            d2[d[j]]=list[i]
+L = sorted(d.items(), key=lambda x:x[1])   #指定按那个元素进行排序
+print('L:',L)
+
+for i in L:
+    j = i[0]
+    d2[j] = i[1]
 print('d2:',d2)
-
-
